4174c0ac86568b742d8f43b7a718b34db7c2a8be
Ernest Koe <ernest.koe@proofgroup.com>
2020-01-27 10:55:08 -0500

    added code to repopulate collection

--- backend/eventbriteUtils.js
+++ backend/eventbriteUtils.js
@@ -20,7 +20,7 @@ function Parselist(result) {
           let name = result.attendees[i].profile.name;
           let __company = result.attendees[i].profile.company;
           let company = (typeof __company === 'undefined' ) ? 'N/A' : __company;
-          let isAttendee = result.attendees[i].ticket_class_name ==='Attendee Admission' ;
+          let isAttendee = result.attendees[i].ticket_class_name ==='Attendee Admission' && name !== "Remove Remove" ;
           if ( isAttendee ) { 
                 AttList.push({ name, company })
           }
@@ -32,7 +32,7 @@ function Parselist(result) {
      
 };
 
-function Poelist(continuation='', acc = []) {
+export function Poelist(continuation='', acc = []) {
 
      let url = (continuation === "") ?
           API_URL :
@@ -79,15 +79,34 @@ Poelist()
       const attcount = myArray.length;
 	 var html = '<div class="attcount">' + attcount + ' attendees</div>' + '<ul class="list">' + myArray.map(function (element) {
           let company = (typeof element["company"] === 'undefined') ? 'N/A': element["company"];
-          if ( element["name"] !== "Remove Remove") {
-                    return '</div><li><div class="list-item"><div class="list-name">' + element["name"] +  
+          let name = element["name"];
+          return '</div><li><div class="list-item"><div class="list-name">' + element["name"] +  
                     '</div><div class="list-company">' + company +  '</div></div></li>'
-               }
      }).join('') + '</ul>';
 	   //$w("#attendeelist").postMessage(html);
-	   console.log(html);
+	   //console.log(html);
 	      // Logs: 20
 	})
 	.catch(error => {
 		console.log(error);
-	})
+     })
+     
+
+export function updateCollection() {
+	wixData.truncate("STLAttendee_2").then( () => {
+
+	Poelist().then(myList => {
+		wixData.bulkInsert("STLAttendee_2", myList)
+			.then((results) => {
+				let inserted = results.inserted; // 2
+				let insertedIds = results.insertedItemIds; // ["00001", "00002"]
+				let updated = results.updated; // 0
+				let skipped = results.skipped; // 0
+				let errors = results.errors; // []
+			})
+			.catch((err) => {
+				let errorMsg = err;
+			});
+	});
+	});
+}
